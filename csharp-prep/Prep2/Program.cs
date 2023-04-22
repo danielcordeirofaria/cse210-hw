bafe262b4ecfa18b6b3961de0015d98657b65e25
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-04-22 16:26:07 -0300

    commiting prep1 and prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int intGrade;
+        String grade;
+
+        Console.Write("What is your number grade? ");
+
+        grade = Console.ReadLine();
+
+        intGrade = int.Parse(grade);
+
+        if(intGrade < 60){
+            Console.WriteLine("Your letter grade is F");
+        }
+        else if(intGrade >= 90){
+            Console.WriteLine("Your letter grade is A");
+        }
+        else if(intGrade >= 80){
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if(intGrade >= 70){
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if(intGrade >= 60){
+            Console.WriteLine("Your letter grade is D");
+        }
     }
 }
