1ff9ffaabdff419860b895e53a02d41dc6294487
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-05-04 15:42:17 -0300

    Week_03 done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,11 +6,26 @@ class Program
     {
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Itau Bank";
+        job1._company = "Microsoft";
         job1._startYear = "2022";
         job1._endYear = "2023";
 
-        job1.Display();
 
+        Job job2 = new Job();
+        job2._jobTitle = "QA";
+        job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Daniel Faria";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Dispay();
     }
 }
